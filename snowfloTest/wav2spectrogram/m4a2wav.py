65d2df7431b94464119f66a7c60e975fa71766eb
snowflo <g175151@gmail.com>
2021-02-02 16:07:42 +0900

    m4a2wav test: 로컬 폴더내에서만 작업이라, firebase와는 고려 필요

---
+++ snowfloTest/wav2spectrogram/m4a2wav.py
@@ -0,0 +1,42 @@
+# Convert all file extensions to m4a (if required)
+'''
+import os, sys
+
+folder = 'D:/DDataSet/living_light_on/' # D:\DDataSet\living_light_on
+
+for filename in os.listdir(folder):
+    infilename = os.path.join(folder,filename)
+    if not os.path.isfile(infilename): continue
+    oldbase = os.path.splitext(filename)
+    newname = infilename.replace('.tmp','.m4a')
+    output = os.rename(infilename, newname)
+'''
+
+# 폴더이름: D드라이브 DDataSet
+
+import os
+import argparse
+
+from pydub import AudioSegment
+
+formats_to_convert = ['.m4a']
+
+for (dirpath, dirnames, filenames) in os.walk("D:/DDataSet/living_light_on/"):
+    for filename in filenames:
+        if filename.endswith(tuple(formats_to_convert)):
+
+            filepath = dirpath + '/' + filename
+            (path, file_extension) = os.path.splitext(filepath)
+            file_extension_final = file_extension.replace('.', '')
+
+            try:
+                track = AudioSegment.from_file(filepath,
+                                               file_extension_final)
+                wav_filename = filename.replace(file_extension_final, 'wav')
+                wav_path = dirpath + '/' + wav_filename
+                print('CONVERTING: ' + str(filepath))
+                file_handle = track.export(wav_path, format='wav')
+                os.remove(filepath)
+            except:
+                print("ERROR CONVERTING " + str(filepath))
+
