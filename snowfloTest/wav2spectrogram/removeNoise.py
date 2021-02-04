cc9068c700682c112f770d0ff56c303fec89d6af
snowflo <g175151@gmail.com>
2021-02-04 18:08:31 +0900

    wav2spec, remove noise 수정

--- snowfloTest/wav2spectrogram/removeNoise.py
+++ snowfloTest/wav2spectrogram/removeNoise.py
@@ -2,11 +2,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-folder_src = 'gpng/fig'
-folder_dst = 'gnoise/gfig'
+file = "D:/DDataSet/spectrogram/"
+folder_src = file+'tpng/fig'
+folder_dst = file+'tnoise/ofig'
 FIG_SIZE = (0.5,0.5)
 
-for i in range(1,30):
+for i in range(1,161):
     img = cv2.imread(folder_src+str(i)+'.png')
 
     dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
@@ -24,10 +25,10 @@ for i in range(1,30):
     plt.tight_layout()
     plt.subplots_adjust(left=0, bottom=0, right=1, top=1, hspace =0, wspace=0)
 
-    plt.imshow(img, cmap=plt.cm.rainbow, interpolation='bicubic')
-    plt.savefig("gnoise/noise"+str(i)+".png", dpi = 300, facecolor = 'none')
+    #plt.imshow(img, cmap=plt.cm.rainbow, interpolation='bicubic')
+    #plt.savefig(file+"gnoise/noise"+str(i)+".png", dpi = 300, facecolor = 'none')
     plt.imshow(dst, cmap=plt.cm.rainbow, interpolation='bicubic')
-    plt.savefig("gnoise/unnoise"+str(i)+".png", dpi = 300, facecolor = 'none')
+    plt.savefig(file+"tnoise/tunnoise"+str(i)+".png", facecolor = 'none')
 
     print("remove noise "+str(i))
 
--- snowfloTest/wav2spectrogram/wav2spectrogram.py
+++ snowfloTest/wav2spectrogram/wav2spectrogram.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 FIG_SIZE = (0.5,0.5) # FIG_SIZE * DPI
 
 file = "D:/DDataSet/"
-for i in range(1,30):
-    sig, sr = librosa.load(file +"living_light_on/g"+str(i)+".wav", sr=22050) # sampling rate 샘플링의 속도 ; 샘플링이란 아날로그 데이터에서 디지털 데이터를 추출하는 것 ; 즉 1초에 얼마나 읽는지 샘플링 레이트
+for i in range(1,161):
+    sig, sr = librosa.load(file +"tv_on/t ("+str(i)+").wav", sr=22050) # sampling rate 샘플링의 속도 ; 샘플링이란 아날로그 데이터에서 디지털 데이터를 추출하는 것 ; 즉 1초에 얼마나 읽는지 샘플링 레이트
 
     print(sr)
     print(sig, sig.shape)
@@ -20,7 +20,7 @@ for i in range(1,30):
     P = librosa.power_to_db(mel, ref=np.max)
     librosa.display.specshow(P)
 
-    plt.savefig('gpng/fig' + str(i) + '.png', bbox_inches=None, pad_inches=0)
+    plt.savefig(file+'spectrogram/tpng/fig' + str(i) + '.png', bbox_inches=None, pad_inches=0)
 
     '''
     librosa.display.waveplot(sig, sr, alpha=0.5)
